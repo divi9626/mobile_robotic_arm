ddd33769f0d476ba60cd9e05cd40f582e0e6e496
Divyam G <dgarg1@umd.edu>
2020-12-07 03:45:12 -0500

    creating backup of package

---
+++ test_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/divyam/test_ws/src
+# Build directory: /home/divyam/test_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(main_assem)
---
+++ test_ws/build/main_assem/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/divyam/test_ws/src/main_assem
+# Build directory: /home/divyam/test_ws/build/main_assem
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
